b42e1694d45cc09e42100a38f9e4c731bf271164
Greem4 <i@greem4.ru>
2023-11-07 23:09:44 +0300

    R 160, Let's do refactoring and talk about class structures.

--- src/main/java/edu/javacourse/student/buisness/StudentOrderService.java
+++ src/main/java/edu/javacourse/student/buisness/StudentOrderService.java
@@ -54,7 +54,7 @@ public class StudentOrderService {
     public void testGet() {
         List<StudentOrder> sos = dao.findAll();
         LOG.info(sos.get(0).getWife().getGivenName());
-        LOG.info(sos.get(0).getChildren().get(0).getGivenName());
+        LOG.info(sos.get(0).getChildren().get(0).getChild().getGivenName());
     }
 
     private Adult buildPerson(boolean wife) {
@@ -96,7 +96,16 @@ public class StudentOrderService {
         StudentOrderChild p = new StudentOrderChild();
         p.setStudentOrder(so);
 
-        p.setDateOfBirth(LocalDate.now());
+        Child c = new Child();
+        c.setDateOfBirth(LocalDate.now());
+        c.setSurName("Рюрик");
+        c.setGivenName("Дмитрий");
+        c.setPatronymic("Иванович");
+
+        c.setCertificateDate(LocalDate.now());
+        c.setCertificateNumber("BIRTH N");
+        c.setRegisterOffice(daoRegister.getOne(1L));
+
         Address a = new Address();
         a.setPostCode("190000");
         a.setBuilding("21");
@@ -104,15 +113,9 @@ public class StudentOrderService {
         a.setApartment("199");
         Street one = daoStreet.getOne(1L);
         a.setStreet(one);
-        p.setAddress(a);
+        c.setAddress(a);
 
-        p.setSurName("Рюрик");
-        p.setGivenName("Дмитрий");
-        p.setPatronymic("Иванович");
-
-        p.setCertificateDate(LocalDate.now());
-        p.setCertificateNumber("BIRTH N");
-        p.setRegisterOffice(daoRegister.getOne(1L));
+        p.setChild(c);
 
         return p;
     }
