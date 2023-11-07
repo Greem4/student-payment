44bd598d5c55421ab1d5eb68db8b6aadfe4537e9
Greem4 <i@greem4.ru>
2023-11-07 17:59:40 +0300

    R 158 Add directories to the main table.

--- src/main/java/edu/javacourse/student/domain/Adult.java
+++ src/main/java/edu/javacourse/student/domain/Adult.java
@@ -1,14 +1,17 @@
 package edu.javacourse.student.domain;
 
-import javax.persistence.Embeddable;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.time.LocalDate;
 @MappedSuperclass
 public class Adult extends Person {
     private String passportSeria;
     private String passportNumber;
     private LocalDate issueDate;
-
+    @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)
+    private PassportOffice passportOffice;
+    @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)
+    private University university;
+    private String studentNumber;
     public String getPassportSeria() {
         return passportSeria;
     }
@@ -32,4 +35,29 @@ public class Adult extends Person {
     public void setIssueDate(LocalDate issueDate) {
         this.issueDate = issueDate;
     }
+
+    public PassportOffice getPassportOffice() {
+        return passportOffice;
+    }
+
+    public void setPassportOffice(PassportOffice passportOffice) {
+        this.passportOffice = passportOffice;
+    }
+
+    public University getUniversity() {
+        return university;
+    }
+
+    public void setUniversity(University university) {
+        this.university = university;
+    }
+
+    public String getStudentNumber() {
+        return studentNumber;
+    }
+
+    public void setStudentNumber(String studentNumber) {
+        this.studentNumber = studentNumber;
+    }
+
 }
